2b7592ac0a30b4ea409bbbb650da1ecc99460ac5
tankztz <tankztz@gmail.com>
2019-03-12 20:04:20 +0800

    Bug fixing

--- application/controllers/Words.php
+++ application/controllers/Words.php
@@ -12,13 +12,8 @@ class Words extends CI_Controller {
 	
 	public function index()
 	{
-        $data['title'] = 'ALL WORDS';
         $data['words'] = $this->words_model->get_words();
-		$this->load->view('view/show_words', $data);
-	}
-
-	public function view($key = NULL)
-	{
-			$data['this_word'] = $this->news_model->get_words($key);
+        $data['title'] = 'ALL WORDS';
+		$this->load->view('show_words', $data);
 	}
 }
