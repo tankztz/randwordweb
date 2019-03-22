4a6d619e72c5fafd3d5dde44b1ce1cf5b5de3eec
tankztz <tankztz@gmail.com>
2019-03-23 02:59:06 +0800

    Improve view page visual

--- application/views/show_words.php
+++ application/views/show_words.php
@@ -1,12 +1,42 @@
+<head>
+<style>
+table {
+  border: 1px solid black;
+  border-collapse: collapse;
+  font-size: 80px;
+  height: 90%;
+  width: 90%;
+  table-layout: fixed ;
+}
+
+th, td {
+  border: 3px solid black;
+  border-collapse: collapse;
+  font-size: 80px;
+  width: 22.5%;
+  padding: 15px;
+  table-layout: fixed;
+}
+</style>
+</head>
+
 <?php 
 echo $title."<br>"; 
 
 $counter = 0;
+$row_num = 5;
 
-foreach ($words as $this_word)
-{
-	$counter++;
-	echo $this_word['word']." ";
-	if ($counter%5 == 0) echo "<br>";
+
+echo "<table style=\"width:100%\">";
+while ($counter < 25){
+	foreach ($words as $this_word)
+	{
+		$counter++;
+		if ($counter%5 == 1) echo "<tr>";
+		echo "<td>".$this_word['word']."</td>";
+		if ($counter%5 == 0) echo "</tr>";
+		if ($counter == 25) break;
+	}
 }
+echo "</table>";
 
