5a09d8850669b78ecd8b6266bcf04f7a1525d3bc
tankztz <tankztz@gmail.com>
2019-03-23 02:06:44 +0800

    Add limit in words model

--- application/models/Words_model.php
+++ application/models/Words_model.php
@@ -1,20 +1,24 @@
 <?php
 class Words_model extends CI_Model {
 
-    public function __construct()
-    {
-        $this->load->database();
+	public function __construct()
+	{
+		$this->load->database();
 	}
-	
-    public function get_words($id = FALSE)
-    {
-        if ($id === FALSE)
-        {
-            $query = $this->db->get('allwords');
-            return $query->result_array();
-        }
-        $query = $this->db->get_where('allwords', array('word' => $id));
-        return $query->row_array();
+
+	public function get_words($id = FALSE, $seed = FALSE)
+	{
+		if ($seed === FALSE)
+		{
+			$query = $this->db->query('SELECT word FROM allwords LIMIT 10');
+			return $query->result_array();
+		}
+		else
+		{
+			//TODO
+			$query = $this->db->get_where('allwords', array('word' => $id));
+			return $query->row_array();
+		}
 	}
 
 }
