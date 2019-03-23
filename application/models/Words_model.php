0511143e986b8acd9f1d2e143f38ec5a68261976
tankztz <tankztz@gmail.com>
2019-03-23 19:00:18 +0800

    Add random words feature

--- application/models/Words_model.php
+++ application/models/Words_model.php
@@ -6,19 +6,46 @@ class Words_model extends CI_Model {
 		$this->load->database();
 	}
 
-	public function get_words($id = FALSE, $seed = FALSE)
+	public function get_words($seed = FALSE, $num = 25)
 	{
-		if ($seed === FALSE)
+		if ($seed == FALSE)
 		{
-			$query = $this->db->query('SELECT word FROM allwords LIMIT 10');
-			return $query->result_array();
+			$seed = mt_rand(0, 9999)*10000 + 13;
 		}
-		else
+
+		if ($seed < 1000) throw new Exception('Invalid Seed.');
+
+		//seed correct here
+		$total = $seed % 10000;
+		$trueSeed = $seed / 10000;
+		$sql = 'SELECT word FROM allwords WHERE ';
+		$output = [];
+		for ($i = 0; $i < $num; $i++)
 		{
-			//TODO
-			$query = $this->db->get_where('allwords', array('word' => $id));
-			return $query->row_array();
+			$thissql = $sql;
+			$output = array_merge($output, $this->get_word_with_id($trueSeed % $total + 1));
+			echo $i." ".$trueSeed % $total." <\br>";#echo $output[1];
+			$trueSeed = $this->next_seed($trueSeed);
 		}
+		return $output;
 	}
 
+	public function get_word_with_id($id)
+	{
+		if ($id === FALSE) throw new Exception('id is FALSE.');
+
+		$query = $this->db->query('SELECT word FROM allwords WHERE id='.$id);
+		return $query->result_array();
+	}
+
+	public function next_seed($seed)
+	{
+		$seed *= 997;
+		$seed += 12345;
+		$seed %= 1000007;
+		$seed *= 997;
+		$seed += 12345;
+		$seed %= 1000007;
+		return $seed;
+	}
 }
